0eaf134016014fca8454597425e96cfe6e10a58b
AbdurRahman-34 <get.abdurahman@gmail.com>
2023-11-02 20:22:43 +0600

    events js

---
+++ class-14-JavaScript-Events/events.js
@@ -0,0 +1,48 @@
+/* JavaScript Events ::::::::::
+
+HTML events are "things" that happen to HTML elements.
+-এইচটিএমএল ইভেন্টগুলি হল "জিনিস" যা এইচটিএমএল উপাদানগুলির সাথে ঘটে৷
+When JavaScript is used in HTML pages, JavaScript can "react" on these events.
+যেমন বাটন হোভার করলে কি ঘটবে তা ইভেন্ট মাধ্যমে দেওয়া যায় 
+
+Here are some examples of HTML events:
+An HTML web page has finished loading
+An HTML input field was changed
+An HTML button was clicked
+
+
+
+
+Common HTML Events
+Here is a list of some common HTML events:
+Event	Description :::::::::
+
+-onchange	An HTML element has been changed
+
+-onclick	The user clicks an HTML element
+
+-onmouseover	The user moves the mouse over an HTML element
+
+-onmouseout	The user moves the mouse away from an HTML element
+
+-onkeydown	The user pushes a keyboard key
+
+-onload	The browser has finished loading the page
+
+
+জাভাস্ক্রিপ্ট ইভেন্ট হ্যান্ডলার :::::
+ইভেন্ট হ্যান্ডলারগুলি ব্যবহারকারীর ইনপুট, ব্যবহারকারীর ক্রিয়াকলাপ এবং ব্রাউজার ক্রিয়াগুলি পরিচালনা এবং যাচাই করতে ব্যবহার করা যেতে পারে:
+
+প্রতিবার পেজ লোড হওয়ার সময় যে কাজগুলো করা উচিত
+পেজ বন্ধ হলে যে কাজগুলো করা উচিত
+একজন ব্যবহারকারী একটি বোতামে ক্লিক করলে যে কাজটি করা উচিত
+কোনো ব্যবহারকারী ডেটা ইনপুট করার সময় যে বিষয়বস্তু যাচাই করা উচিত
+এবং আরো...
+ইভেন্টগুলির সাথে জাভাস্ক্রিপ্টকে কাজ করতে দেওয়ার জন্য অনেকগুলি বিভিন্ন পদ্ধতি ব্যবহার করা যেতে পারে:
+
+HTML ইভেন্ট অ্যাট্রিবিউট সরাসরি জাভাস্ক্রিপ্ট কোড এক্সিকিউট করতে পারে
+HTML ইভেন্ট অ্যাট্রিবিউট জাভাস্ক্রিপ্ট ফাংশন কল করতে পারে
+আপনি HTML উপাদানগুলিতে আপনার নিজস্ব ইভেন্ট হ্যান্ডলার ফাংশন বরাদ্দ করতে পারেন
+আপনি ইভেন্টগুলি পাঠানো বা পরিচালনা করা থেকে আটকাতে পারেন
+এবং আরো...
+*/
